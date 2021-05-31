61e7196fd0266706b96e1ef79361e964df040366
maryam akbari <maryamakbarii2120@gmail.com>
2021-06-01 00:15:46 +0430

    first commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/LibClass.java
@@ -0,0 +1,10 @@
+package com.example.mylibrary;
+
+public class LibClass {
+
+    public int calculateSum(int fNumber,int sNumber){
+        int sum;
+        sum=fNumber+sNumber;
+        return sum;
+    }
+}
